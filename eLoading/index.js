596cf89ab3afebd0638c47bca6c39383a8fc5efb
lijixuan <15201002062@163.com>
2022-11-12 20:41:34 +0800

    feat: 增加按钮loading

--- eLoading/index.js
+++ eLoading/index.js
@@ -2,9 +2,9 @@
  * @Description: 
  * @Author: 李大玄
  * @Date: 2022-11-12 17:44:02
- * @FilePath: /vue-shelf/src/views/testLoading/loading/eLoading/index.js
+ * @FilePath: /ldx-loading/eLoading/index.js
  * @LastEditors: 李大玄
- * @LastEditTime: 2022-11-12 18:01:52
+ * @LastEditTime: 2022-11-12 20:41:05
  */
 let BUTTONNDOM = null;
 let context = '';
@@ -12,21 +12,19 @@ function getNode(event) {
   let domlist = Array.from(event.path);
   for (let i = 0; i < domlist.length; i++) {
     if (domlist[i].tagName == "BUTTON") {
-
       return domlist[i]
     }
   }
-
 }
 const show = (params) => {
-  BUTTONNDOM.setAttribute("style", "cursor: default; pointer-events: none;");
+  BUTTONNDOM.setAttribute("style", "cursor: default; pointer-events: none;position: relative;");
   BUTTONNDOM.blur();
   BUTTONNDOM.className += ' is-loading';
   context = BUTTONNDOM.innerHTML;
   BUTTONNDOM.innerHTML = '<i class="el-icon-loading"></i>' + context;
 }
 const close = (params) => {
-  BUTTONNDOM.setAttribute("style", "cursor: pointer; pointer-events: auto;");
+  BUTTONNDOM.setAttribute("style", "cursor: pointer; pointer-events: auto;position: relative;");
   BUTTONNDOM.classList.remove("is-loading");
   BUTTONNDOM.innerHTML = context;
   BUTTONNDOM = null;
